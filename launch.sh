a608cabb665d48d55f1ba07078cd038a99845716
Alois Colleaux-Le Chene <alois.colleaux-le-chene@epita.fr>
2021-11-24 16:49:36 +0100

    a

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 nix profile install nixpkgs#wine
-nix profile install nixpkgs#xdotools
+nix profile install nixpkgs#xdotool
 cd /tmp
 rm DesktopGoose.zip
 wget https://raw.githubusercontent.com/Helyosis/rigolo/main/DesktopGoose.zip
