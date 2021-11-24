bc65b9f20d8daee81e082f473445373a336dd02e
Alois Colleaux-Le Chene <alois.colleaux-le-chene@epita.fr>
2021-11-24 09:40:18 +0100

    Updated script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 nix profile install nixpkgs#wine
 cd /tmp
-wget https://github.com/Helyosis/rigolo/blob/main/DesktopGoose.zip
+rm DesktopGoose.zip
+wget https://github.com/Helyosis/rigolo/raw/main/DesktopGoose.zip
 unzip DesktopGoose.zip
 wine DesktopGoose/GooseDesktop.exe &
