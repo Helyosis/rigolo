6988d3a7b41fbff269be204ce7632f1b613eefcd
Alois Colleaux-Le Chene <alois.colleaux-le-chene@epita.fr>
2021-11-24 17:01:09 +0100

    Bruh

--- launch.sh
+++ launch.sh
@@ -2,8 +2,15 @@ nix profile install nixpkgs#wine
 nix profile install nixpkgs#xdotool 
 cd /tmp
 rm DesktopGoose.zip
+rm -r DesktopGoose
 wget https://raw.githubusercontent.com/Helyosis/rigolo/main/DesktopGoose.zip
 unzip DesktopGoose.zip
 wine DesktopGoose/GooseDesktop.exe &
 sleep 25
 xdotool key Super_L+f
+
+for i in {1..50};
+do  wine DesktopGoose/GooseDesktop.exe &
+    sleep 4
+    xdotool key Super_L+f
+done;
